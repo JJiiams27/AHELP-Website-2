b1421f5283843c8051d1bfd0f49201839866eeb6
JJiiams27 <iiamsj@csp.edu>
2025-08-06 14:36:35 -0500

    refactor: extract shared navigation and footer

---
+++ build.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const inputDir = path.join(__dirname, 'improved-website-v14');
+const outputDir = path.join(__dirname, 'dist');
+
+const nav = fs.readFileSync(path.join(inputDir, 'partials', 'nav.html'), 'utf8');
+const footer = fs.readFileSync(path.join(inputDir, 'partials', 'footer.html'), 'utf8');
+
+function render(content) {
+  return content
+    .replace(/{% include "partials\/nav.html" %}/g, nav)
+    .replace(/{% include "partials\/footer.html" %}/g, footer);
+}
+
+fs.mkdirSync(outputDir, { recursive: true });
+const entries = fs.readdirSync(inputDir, { withFileTypes: true });
+for (const entry of entries) {
+  const srcPath = path.join(inputDir, entry.name);
+  const outPath = path.join(outputDir, entry.name);
+  if (entry.isDirectory()) {
+    if (entry.name === 'partials') continue; // skip partials
+    fs.cpSync(srcPath, outPath, { recursive: true });
+  } else if (entry.name.endsWith('.html')) {
+    const content = fs.readFileSync(srcPath, 'utf8');
+    const rendered = render(content);
+    fs.writeFileSync(outPath, rendered);
+  } else {
+    fs.copyFileSync(srcPath, outPath);
+  }
+}
